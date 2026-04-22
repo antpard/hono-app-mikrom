880498f020a24dd43e1b39e2aab2b60717835d50
Antonio Pardo <70755911+antpard@users.noreply.github.com>
2026-04-23 01:15:51 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,11 @@ serve({
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
+
+const port = Number(process.env.PORT) || 3000;
+serve({
+  fetch: app.fetch,
+  port: port
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
