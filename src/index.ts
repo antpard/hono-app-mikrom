a1243023deb9f00d224ef32cbd1665e75f82fa19
Antonio Pardo <apardo@spluca.org>
2026-04-24 13:49:03 +0200

    change string

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Mikrom from GitHub Push!')
 })
 
 const port = Number(process.env.PORT) || 3000;
